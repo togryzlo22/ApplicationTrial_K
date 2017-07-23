9334c7df5bf09cb4b51d8d354563eb92571ce5c9
Windows <toddo@hotmail.ca>
2017-07-22 22:04:10 -0700

    nice changes iwht images

--- Memberships/Controllers/HomeController.cs
+++ Memberships/Controllers/HomeController.cs
@@ -12,22 +12,14 @@ namespace Memberships.Controllers
 {
     public class HomeController : Controller
     {
-        public async Task<ActionResult> Index()
+        public ActionResult Index() 
         {
-            var userId = Request.IsAuthenticated ? HttpContext.User.Identity.GetUserId() : null;
-            var thumbnails = await new List<ThumbnailModel>().GetProductThumbnailsAsync(userId);
-            var count = thumbnails.Count() / 4;
-            var model = new List<ThumbnailAreaModel>();
-            for (int i = 0; i <= count; i++)
-            {
-                model.Add(new ThumbnailAreaModel
-                {
-                    Title = i.Equals(0) ? "My Content" : string.Empty,
-                    Thumbnails = thumbnails.Skip(i * 4).Take(4)
-                });
-            }
-            return View(model);
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+
         }
+       
 
         public ActionResult About()
         {
---
+++ Memberships/Controllers/ResumeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Memberships.Controllers
+{
+    public class ResumeController : Controller
+    {
+        // GET: Resume
+        public ActionResult Education()
+        {
+            return View();
+        }
+
+        public ActionResult Experience()
+        {
+            return View();
+        }
+
+
+        public ActionResult Porfolio()
+        {
+            return View();
+        }
+
+
+      public ActionResult Skills()
+        {
+
+
+            return View();
+        }
+
+        public ActionResult Character()
+        {
+
+            return View();
+        }
+
+
+
+
+    }
+}
